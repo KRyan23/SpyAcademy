eb4f94ea450e83ff8823bf71f7c144ea6700bdcd
KRyan23 <GitHub@kryan23.bulc.club>
2021-05-24 18:02:23 +0100

    index.html + index.css + index.js Removed input box for age and added slider instead, still some styling to complete on slider.

---
+++ assets/scripts/index.js
@@ -0,0 +1,29 @@
+
+
+
+function show_value(x)
+{
+ document.getElementById("age-slider").innerHTML=x;
+}
+
+/*function updateTextInput(val) {
+          //document.getElementById("age-slider-entry").value=val;
+          document.getElementById("age-slider").innerHTML=val;
+        }
+*/
+
+
+/*
+var g = document.getElementById("age-input").addEventListener('input', enterGame);
+
+function enterGame(){
+let g;
+if (Number.isInteger(g)){
+console.log("your old enough");
+}else{
+
+console.log("Your age is", g);
+}
+}
+enterGame();
+*/
