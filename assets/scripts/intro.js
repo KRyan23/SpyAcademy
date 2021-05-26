8921199cae9a1aa6b9380ea1b5ad6266816aa0f3
KRyan23 <GitHub@kryan23.bulc.club>
2021-05-26 20:34:14 +0100

    index.html + intro.js + game1.html - tidied up index.html,   created intro.js for testing,  test code on game1.html.

---
+++ assets/scripts/intro.js
@@ -0,0 +1,33 @@
+
+setTimeout(function(){
+  let x = 10000;
+  x--;
+  let y = document.getElementById('timer');
+  x = y.innerHTML;
+}, 1000);
+
+
+
+
+/*
+//setInterval(function(){
+alert("Hello");
+}, 3000);
+
+
+setInterval(function(){
+  let timerElement = document.getElementById('timer');
+
+  let currentTime = timerElement.innerHTML;
+  currentTime++;
+  timerElement.innerHTML = currentTime;
+}, 1000);
+
+function countdown(){
+  let x = 10000;
+  let z = document.getElementById('timer');
+  z--;
+  z.innerHTML = x;
+}
+function countdown();
+*/
