6adc13b4f113994c902f181e87e525f71256ea9c
KRyan23 <GitHub@kryan23.bulc.club>
2021-05-26 13:06:11 +0100

    index.html + index.js + game1.html - tidied up index.js, spelling corrections on index.html, only test code on game1.html, updated readme with sourced content.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,26 +1,23 @@
 
 
-function show_value(x)
-{
- document.getElementById("age-slider").innerHTML=x;
- karl();
- karl2();
+// To display the age chosen above the slider
+function show_slider_age(x){
+  document.getElementById("age-slider").innerHTML=x;
+  brb_active();
+
 }
 
-/*
-function buttonActive(){
-If (x >= 6){
- var element = document.getElementById("myDIV");
- element.classList.remove("game-active");
-}
-*/
-function karl(){
-  var element = document.getElementById("link-for-brb");
-  element.classList.remove("button-not-active");
+// Make the BRB flash and activate the link
+function brb_active(){
+  var x = document.getElementById("link-for-brb");
+  x.classList.remove("button-not-active");
+
+  var y = document.getElementById("pulsing-background");
+  y.classList.remove("main-page-text-age");
+
+  var z = document.getElementById("big-red-button");
+    z.classList.add("main-button-active");
+
 }
 
-
-function karl2(){
-  var element = document.getElementById("big-red-button");
-    element.classList.add("main-button-active");
-}
+//End of file
