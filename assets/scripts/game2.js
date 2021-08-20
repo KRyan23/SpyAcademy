e001757c85c48c7e0adaf1152a0aef9ea2ed60d3
KRyan23 <GitHub@kryan23.bulc.club>
2021-08-20 20:48:06 +0100

    Game1 all files only removed white space and added a few comments. Game2 all files not happy with interface or implementation redoing a lot of it.

--- assets/scripts/game2.js
+++ assets/scripts/game2.js
@@ -2,82 +2,83 @@
 startGame();
 
 
-//This is the jquery code to toggle the instructions on the game, some css manipulation was needed.
-ToggleInstructions();
-function ToggleInstructions(){
-$( "#game-instructions" ).click(function() {
-    $( "#text-instructions" ).toggle();
-    let inst = $('#game-instructions').html();
-    if (inst == "Instructions"){
-      $('#game-instructions').html('Hide Instructions').css("font-size", "1em");
-    }else{
-      $('#game-instructions').html('Instructions').css("font-size", "1em");
-    }
-});
+function generatePassword(){
+//Password Bank in a nested array to choose a word from.
+let passwordBank = [['T','R','I','U','M','P','H','A','N','T','L','Y'], ['C','O','M','B','I','N','A','T','I','O','N'], ['S','T','R','E','N','G','T','H','E','N','I','N','G'], ['G','R','A','T','E','F','U','L','N','E','S','S'], ['C','O','M','P','U','T','E','R','L','E','S','S'], ['M','I','C','R','O','M','A','C','H','I','N','E','S'], ['S','E','C','R','E','T','I','V','E','L','Y'], ['P','R','I','V','A','T','E','L','Y'], ['A','V','A','L','A','N','C','H','E','S'], ['F','O','O','L','P','R','O','O','F','E','D'],['S','T','R','O','N','G','H','O','L','D'], ['I','M','P','O','S','S','I','B','L','E'], ['C','O','N','C','E','N','T','R','A','T','E'], ['C','L','A','S','S','I','F','I','E','D'], ['U','N','D','E','R','C','O','V','E','R'], ['A','B','R','A','C','A','D','A','B','R','A'], ['C','A','M','O','F','L','A','G','E','D'], ['M','A','S','T','E','R','P','L','A','N'], ['F','A','C','E','L','E','S','S','N','E','S','S','E','S'], ['C','R','A','C','K','A','B','L','E'], ['H','A','C','K','A','B','I','L','I','T','Y'], ['M','I','L','L','I','O','N','A','I','R','E','S'], ['C','R','Y','P','T','O','G','R','A','P','H','Y'], ['M','E','S','S','E','N','G','E','R','S'], ['D','E','P','E','N','D','A','B','L','E'], ['V','A','C','C','I','N','A','T','I','O','N'], ['K','A','L','E','I','D','O','S','C','O','P','E'], ['A','E','R','O','D','Y','N','A','M','I','C'], ['L','A','N','D','F','O','R','T','R','E','S','S'], ['A','B','O','V','E','G','R','O','U','N','D'], ['T','A','S','K','M','A','S','T','E','R'], ['A','E','R','O','P','L','A','N','E','S'],['A','D','V','E','N','T','U','R','E'],['U','L','T','R','A','C','A','R','E','F','U','L','L'],['T','A','N','G','E','R','I','N','E','S']]
+
+let x = (Math.floor(Math.random() * passwordBank.length));                   //Store a random number of < array length from the password bank to variable 'x'.
+let passLength = passwordBank[x].length;                                     //Find out the length of the password to be used for later displaying hints.
+var privatePassword = passwordBank[x];                                       //Assign privatePassword the output from passwordBank.
+let publicPassword = Array.from(privatePassword).sort().reverse().join("");  //Clone the privatePassword value so it doesnt alter the orginal, alphabetaically sort then reverse to make it mor difficult to guess, also remove the commas from the word.
+localStorage.setItem("passlength", JSON.stringify(passLength));              //Stores the length of the guessed password in local storage.
+localStorage.setItem("privatepassword", [privatePassword]);                  //Stores the length of the password to be guessed into local storage.
+localStorage.setItem("publicpass", [publicPassword]);                        //Stores the length of the jumbled up version of the actual password in local storage.
 }
 
-var winflag;
-// Dont like using global variables but necessary in this case.
 
-/*tryCombination only executes after the startGame function is running */
-  function tryCombination(){
-    trys--;
-    if (trys == 0){
-      stopGame();
-      document.getElementById("abort-message-show").removeAttribute("style");
-    }else if(trys <20 && trys >10){
-      document.getElementById("attempts").innerHTML = "00" + trys; //Pads the start with 2 zeros for effect
-      document.getElementById("attempts").style.cssText = "color:orange; opacity: 0.75"; //sets to orange for running low warning
-    }else if(trys <10){
-      document.getElementById("attempts").innerHTML = "000" + trys; //Pads the start with 3 zeros when trys < 10
-      document.getElementById("attempts").style.cssText = "color:red; opacity: 0.85"; //sets to red for < 10 try left warning
-    }else{
-      document.getElementById("attempts").innerHTML = "00" + trys;
+function populateKeys(){
+  let pass = localStorage.getItem("publicpass");                            //Get the public password from local storage.
+  let len = pass.length;                                                    //Retrieve the length of the password
+  let letters = 0, keyId = 0;                                               //Let variables 'letters' and the 'keyId' start at 0
+  for(let i = 0; i < pass.length; i++){                                     //loop through each id and only assign the keys the same ammount of letters that are in the password
+    let name = "key-" + keyId;
+    document.getElementById(name).innerText = pass[letters]; //Populate the keys with the necessary letters.
+    keyId++;
+    letters++;
     }
+// Create and Populate password entry boxes with 'x'///
+setTimeout(() => {
+  for(let j=0; j < pass.length; j++){
+    document.getElementById('password-boxes').insertAdjacentHTML('beforebegin', `<div class="charBoxes text-center" tabindex=${j} id="char-box-${j}">X</div>`); //The syntax for this command was found at https://www.w3schools.com/jsref/met_node_insertadjacenthtml.asp
 
-    if(combo[0] === combo[6] && combo[1] === combo[7] && combo[2] === combo[8] && combo[3] === combo[9] && combo[4] === combo[10] && combo[5] === combo[11]){
-      winflag = 1;                                                  //Sets the winflag to 1 to be quiered by the setInterval function
-      nextGame();                                                   //Ends game if sucessfull calls the nextGame function.
-          }else{
-      winflag = 0;
-    }
+
+
+}
+}, 4000);/* Wait 4s to display the abort/loose message */
+
 }
 
-var trys;
-// Dont like using global variables but necessary in this case.
-
-/* Sets the difficuly based on the age of the participant and calls the tryCombination
-function that monitors the remaining attempts, if the attempts reach 0 it calls stopGame.*/
+/* This sets the difficuly based on the age of the participant and updates the initial ammount of attempts*/
 function setDifficulty(){
 let level = localStorage.getItem("age");
-
+  var trys;
   if (level >= 6 && level <= 8){
-    trys = 30; // 36 attempts after 1st click
+    trys = 30; //  30 attempts after 1st click
   }else if (level >= 9 && level <= 12) {
-    trys = 25; // 30 attempts after 1st click
-  }else if (level > 12){
-     trys = 20; // 24 attempts after 1st click
+    trys = 25; //  25 attempts after 1st click
+  }else if (level => 12){
+     trys = 20; //  20 attempts after 1st click
   }else{
-
+    alert("You did not choose an age");
   }
+
+  localStorage.setItem("attempts",trys)
+
+  document.getElementById("attempts").innerHTML = "00" + trys; // Sets the attempt ammount based on age.
 }
 
-////////////////////////Start Game/////////////////////////////////////////////////////
-/* Listens for a click on the start button, once clicked it runs the function setlevel.
-   Runs the setDifficulty function to monitor attempts and end the game when its reaches 0.
-   Calls the generate Combination function.
-   Bottom 5 rules change various styles on buttons and divs to enhance game play.
-   */
+
+////////////////////////Start Game////////////////////////////////////////////////////////
+/* Listens for a click on the start button, once clicked it runs the functions
+   setDifficulty, generatePassword, populateKeys and stopgame.
+   Bottom 3 rules change various styles on buttons and divs to enhance game play.*/
   function startGame(){
   document.getElementById("start").onclick = function() {
-  setlevel();
   setDifficulty();
-
+  generatePassword();
   document.querySelector("#start").innerHTML = "Abort!";   //Changes the start button text to abort
-  document.getElementById("start").style.cssText = "background:yellow; color:black; opacity:0.6"; //Changes the start button to yellow with black text
-  
-  document.getElementsByClassName("time-trys-left")[0].style.cssText = "opacity:1;"; //changes the 'time' and 'attempts' placeholders to normal opacity
+  document.getElementById("start").style.cssText = "background:yellow; color:black; opacity:1;"; //Changes the start button to yellow with black text
+  document.getElementById("instructions-container").style.cssText = "animation: fadeout 2s forwards;"; // Fades out the instruction text once you hit start
+  setTimeout(() => {
+  document.getElementsByClassName("outer-screen-border")[0].style.cssText = "background-color: #384242; animation: fadein 1s forwards; color: #fafafa;";
+  document.getElementsByClassName("spy-lives-container")[0].style.cssText = "display: contents;";       //Shows mainframe heading
+  document.getElementById("player-progress").style.cssText = "display: contents;";     //Shows the Spy badges and remaining attempts.
+}, 3000);                                                                             // Wait 3s to display the password boxes and to debounce the click for the start/abort button
+  populateKeys(); //Populates the empty keys with the the letters from the chosen password.
 
+  document.getElementById("start").onclick = function() { //Once the Start button changes to 'Abort', this waits for the button to be pressed so it can end the game if needed.
+  stopGame();
+  }
 };
   }
 
@@ -97,60 +98,6 @@ if (level >= 6 && level <= 8){
   time = 122; // 242 - 2minutes
 }
 countdown(time); //Calls the timer function to run for example (180 times), countdown(180)
-}
-
-///////////////////////Countdown/////////////////////////////////////////////////////
-/* Countdown function to time the games based on seconds.
-   Variables seconds and minutes are used to update the html content on screen.*/
-function countdown(seconds) {
-  let counter = seconds;
-  let minutes;
-  let x = 60;
-  let y = 0;
-
-
-  let interval = setInterval(() => {
-    counter--;
-    x--;
-    //Display minutes 4 - 0
-    if (counter < 242 && counter >= 182){minutes = 3;}
-    else if (counter < 182 && counter >= 122){
-      minutes = 2;}
-    else if (counter < 122 && counter >= 62){
-      minutes = 1;
-          document.getElementById("time-remaining").style.cssText = "color:orange; opacity:0.75;"; //Changes the text to orange to signify < 2mins left
-    }else{ minutes = 0;
-          document.getElementById("time-remaining").style.cssText = "color:red; opacity:0.75;";   //Changes the text to red to signify < 1mins left
-        }
-    //Display seconds 0 - 59
-    if(x < 0){x = 59;}
-    //This condition pads the number with a "0" if the seconds are < 10.
-    if (x < 10){
-      document.getElementById("time-remaining").innerHTML = "0" + minutes + ":" + y + x;
-      }
-    //This condition does not pads the number with a "0" if the seconds are > 10.
-    else{
-      document.getElementById("time-remaining").innerHTML = "0" + minutes + ":" + x;}
-    //Terminate the game when counter reaches 1 second left and when winflags value is set to "1"
-    if(counter == 1 && winflag == 1){
-      stopGame();
-      nextGame();
-    //Terminate the game when counter reaches 1 second left and when winflags value is set to "0", then display the abort/loose message.
-    }else if (counter == 1 && winflag == 0){
-        stopGame();
-      document.getElementById("abort-message-show").removeAttribute("style");
-    }
-    //This function waits for the Abort button to be clicked before running the stopGame().
-      document.getElementById("start").onclick = function() {
-
-          setTimeout(() => {/* Wait 300ms to display the abort/loose message*/
-            stopGame();
-          document.getElementById("abort-message-show").removeAttribute("style");
-        }, 300);
-      /* Displays or unhides the Abort message when a user wants to leave a game */
-      };
-
-},1000); //1000ms
 }
 
 /////////////////////////////////Stop Game/////////////////////////////////////////////////////////////
