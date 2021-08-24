36ad5dfd134003cea10d4bb62a8a67c0df2c96b6
KRyan23 <GitHub@kryan23.bulc.club>
2021-08-24 17:56:51 +0100

    Game 3. Added Abort Button and js functionality, sorted out layout on desktop, added wire color function to js file.

--- assets/scripts/game3.js
+++ assets/scripts/game3.js
@@ -1,12 +1,29 @@
 //Start
-document.getElementById("big-red-button").onclick = function() {   //This function waits for the player to click the Big Red button then runs startGame
+//This function waits for the player to click the Big Red button then runs startGame
+document.getElementById("big-red-button").onclick = function() {
 startGame();
 }
+//This function waits for the player to click the Start/Abort button then runs setLevel to begin the game
+document.getElementById("start-abort").onclick = function() {
+
+let quitGame = document.querySelector('#start-abort').innerHTML;          //Queries the text of the button if it matches 'Abort', then it runs stopGame.
+if(quitGame=="Abort!"){
+  console.log(quitGame);
+  stopGame();
+}
+setlevel();                                                             //Calls setlevel which calculate the allowed time based on age, this is then passed to function 'countdown'
+document.querySelector("#start-abort").innerHTML = "Abort!";            //Changes the start button text to abort
+document.getElementById("start-abort").style.cssText = "background:black; color:yellow; opacity:1;"; //Changes the start button to yellow with black tex
+
+}
+//This function scan for the text 'abort' and runs stopGame if the button is clicked
+
 
 function startGame(){                                              //1. Start Game calls hideContent to clear the screen for the main game.
   hideContent();                                                   //2. Calls hideContent which hides the front page.
-  setlevel();                                                      //3. Calls setlevel which calculate the allowed time based on age, this is then passed to function 'countdown'
+
   unhideContent();                                                 //4. Calls unhideContent which displays the contents of the main game.
+  assignButtonColours();                                           //5. Assigns the 'Wire' colors as opposed to using inline styling or multiple classes.
 }
 
 //This function applies a css rule to hide all elements with class 'hide-after-start'
@@ -22,38 +39,42 @@ function unhideContent(){
     document.getElementsByClassName("show-after-start")[j].style.cssText = "animation: fadein 1s;";
     }
 }
+//
 
-function stopGame(interval){
-
-localStorage.setItem('secs', 0); // Sets the Display seconds 0 - 59 If condition to 0 to stop it counting.
-console.log("Game ran out of time");
-
+/* This function displays the 'Unsuccessful' message when either the player runs out of time or hits the 'abort' button */
+function stopGame(){
+for(k=0;k<4;k++){
+  document.getElementsByClassName("show-after-start")[k].style.cssText = "display:none;";     // This reverses the action of the unhidecontent function and hides all game content when stopGame is called..
+}
+ document.getElementsByClassName("outer-screen-border")[0].style.cssText = "display:none;";   // Hides the Outer Screen border
+ document.getElementsByClassName("game-heading")[0].style.cssText = "display:none;";          // Hides the Game Heading
+ document.getElementById("abort-message-show").style.cssText = "display:contents;";           // Shows the abort/unsuccessful message.
 }
 
 //This function sets the allowed time based on age
 function setlevel(){
 let level = localStorage.getItem("age");
 let time;
+
 if (level >= 6 && level <= 8){
-  time = 242; // 242 - 4minutes
+  time = 302; // 242 - 4minutes
 }else if (level >= 9 && level <= 12) {
-  time = 182; // 182 - 3minutes
+  time = 242; // 182 - 3minutes
 }else{
-  time = 122; // 242 - 2minutes
+  time = 182; // 242 - 2minutes
 }
 countdown(time); //Calls the timer function to run for example (180 times), countdown(180)
 }
 
+/* Countdown  */
 function countdown(seconds) {
   let counter = seconds;
   let minutes;
   let x = 60;
   let y = 0;
-  localStorage.setItem('secs', 59);
+
 
   let interval = setInterval(() => {
-
-    let seconds = localStorage.getItem('secs');
     counter--;
     x--;
     //Display minutes 4 - 0
@@ -63,13 +84,17 @@ function countdown(seconds) {
     else if (counter < 122 && counter >= 62){
       minutes = 1;
           document.getElementById("time-remaining").style.cssText = "color:orange; opacity:0.85;"; //Changes the text to orange to signify < 2mins left
-          document.getElementById("flashing-bomb-text").style.cssText = "color:orange; animation:flashdiv 1s infinite; opacity:0.85;"; //Changes the color of the Bomb! text to orange and flashes it every 1s
-        }else{ minutes = 0;
+          document.getElementById("flashing-bomb-text").style.cssText = "color:orange; animation:flashdiv 2s infinite; opacity:0.85;"; //Changes the color of the Bomb! text to orange and flashes it every 1s
+          document.getElementsByClassName("bomb")[0].style.cssText = "border:9px double orange;"; //Increase the border size and sets the border the same color as the clock
+          }else{ minutes = 0;
           document.getElementById("time-remaining").style.cssText = "color:red; opacity:1;";   //Changes the text to red to signify < 1mins left
           document.getElementById("flashing-bomb-text").style.cssText = "color:red; animation:flashdiv 500ms infinite; opacity:1;"; //Changes the color of the Bomb! text to red and flashes it every 0.5s
+          document.getElementsByClassName("bomb")[0].style.cssText = "border:12px double red;"; //Further Increases the border size and sets the border the same color as the clock
         }
     //Display seconds 0 - 59
-    if(x < 0){x = seconds;}
+    if(x < 0 ){
+      x = 59;
+    }
     //This condition pads the number with a "0" if the seconds are < 10.
     if (x < 10){
       document.getElementById("time-remaining").innerHTML = "0" + minutes + ":" + y + x;
@@ -82,11 +107,20 @@ function countdown(seconds) {
       stopGame();
       //nextGame();
     //Terminate the game when counter reaches 1 second left and when winflags value is set to "0", then display the abort/loose message.
-    }else if (counter == 1 && winflag == 0){
+  }else if (counter == 1 ){
         stopGame();
       document.getElementById("abort-message-show").removeAttribute("style");
     }
 
-
 },1000); //1000ms
+}
+
+
+
+
+function assignButtonColours(){
+  let colorChoice= ["blue","brown","gold","green","black","red","purple","orange"];                     //Put the preferred colors in an array called 'colorChoice'
+  for(i=0;i<colorChoice.length;i++){
+    document.getElementsByClassName("wires")[i].style.cssText = `background-color:${colorChoice[i]};`; // Assign a different colour to the each of the wires.
+  }
 }
